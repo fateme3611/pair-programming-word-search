81b6275c9f0633a8806a588b36c99a139dbbe1dc
fateme <fateme3611@yahoo.com>
2021-07-28 16:33:42 -0400

    wordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,24 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = transpose(letters).map(ls=> ls.join(""))
+        for (l of verticalJoin) {
+            if (l.includes(word)) return true
+        
+    }
+    return false;
+}
+
+// transpose for verticalJoin
+const transpose = function (matrix) {
+    //assign new array
+    const result = [];
+    for (let i = 0; i< matrix[0].length; i++) {
+        result.push([]);
+        for (let j = 0; j < matrix.length; j++) {
+            result[i][j] = matrix[j][i]
+        }
+    } return result;
 }
 
 module.exports = wordSearch
